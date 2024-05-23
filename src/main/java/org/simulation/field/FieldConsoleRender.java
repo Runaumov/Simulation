d8fe169f27c154567feae5d704c3883cf5088bf0
Runaumov <casrunau@gmail.com>
2024-05-23 23:40:50 +0300

    change FieldConsoleRender for Windows terminal

--- src/main/java/org/simulation/field/FieldConsoleRender.java
+++ src/main/java/org/simulation/field/FieldConsoleRender.java
@@ -10,7 +10,7 @@ public class FieldConsoleRender {
             for (int x = 1; x <= field.getX(); x++) {
                 Coordinates coordinates = new Coordinates(x, y);
                 if (field.isCellEmpty(coordinates)) {
-                    line += "\u2002" + "\u2022" + "\u2002";
+                    line += " " + "\u2022" + " ";
                 } else {
                     line += getEntitySprite(field.getEntity(coordinates));
                 }
@@ -30,19 +30,19 @@ public class FieldConsoleRender {
     private String getEntitySprite(Entity entity) {
         switch (entity.getClass().getSimpleName()) {
             case "Rabbit":
-                return "\u2004" + "\uD83D\uDC30";
+                return "\uD83D\uDC30" + " ";
             case "Fox":
-                return "\u2004" + "\uD83E\uDD8A";
+                return "\uD83E\uDD8A" + " ";
             case "Grass":
-                return "\u2004" + "\uD83C\uDF3F";
+                return "\uD83C\uDF3F" + " ";
             case "Tree":
-                return "\u2004" + "\uD83C\uDF32" + "\u2006";
+                return "\uD83C\uDF32" + " ";
             case "Stone":
-                return "\u2004" + "\uD83D\uDDFF" + "\u2005" + "\u200A";
+                return "\uD83D\uDDFF" + " ";
             case "Skull":
-                return "\u2004" + "\uD83D\uDC80";
+                return "\uD83D\uDC80" + " ";
             case "Cow":
-                return "\u2004" + "\uD83D\uDC2E";
+                return "\uD83D\uDC2E" + " ";
         }
         return "";
     }
